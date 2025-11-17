c8ed7603831adfddb289ad353508a9fac6d9dc7f
NBILITY <contact@nbility.fr>
2025-11-17 19:12:41 +0100

    Correctif chemin

--- DATA-LOCAL/user-manager/app/public/login.php
+++ DATA-LOCAL/user-manager/app/public/login.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - Bolt.DIY User Manager</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/user-manager/assets/css/style.css">
 </head>
 <body class="login-page">
     <div class="login-container">
